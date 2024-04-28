e33d656f99fe2e26a73b8089cc7faec6ccb490c4
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-04-28 20:53:25 +0600

    removed the base
    will start from skratch

--- app/(user)/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex-1 w-full flex flex-col max-w-6xl mx-auto">
-      {children}
-    </div>
-  );
-}
