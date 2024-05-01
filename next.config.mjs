24a620f62673b1e240bd1cbffa685952c90bd831
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-05-02 00:43:01 +0600

    u

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions: true,
-      },
+
       images: {
         domains: ["github.com", "lh3.googleusercontent.com"],
       },
