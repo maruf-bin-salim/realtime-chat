209fd3eb2ec2e263574486602f9bbe9488f04427
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-05-08 18:08:08 +0600

    u

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -11,7 +11,7 @@ export default function SignUp() {
         provider: "google",
         options: {
           redirectTo: `${location.origin}/auth/callback?next=${
-            ""
+            "/chat"
           }`,
         },
       });
