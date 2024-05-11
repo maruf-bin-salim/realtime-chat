9846f6e38e75f24e74af58317a80538defaf3eba
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-05-12 00:38:34 +0600

    chat group creation done!

--- components/CreateChatButton.tsx
+++ components/CreateChatButton.tsx
@@ -4,35 +4,125 @@ import { MessageSquarePlus } from "lucide-react";
 import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
 // import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useToast } from "./ui/use-toast";
 // import { useSubscriptionStore } from "@/store/store";
 import LoadingSpinner from "./LoadingSpinner";
-// import { v4 as uuidv4 } from "uuid";
-// import {
-//   addChatRef,
-//   chatMembersCollectionGroupRef,
-// } from "@/lib/converters/ChatMembers";
-// import { getDocs, serverTimestamp, setDoc } from "firebase/firestore";
+
 import { ToastAction } from "@radix-ui/react-toast";
+import { createSupabaseBrowserClient } from "@/lib/supabase/browser-client";
+import useSession from "@/lib/supabase/use-session";
 
 function CreateChatButton({ isLarge }: { isLarge?: boolean }) {
   // const { data: session } = useSession();
   const router = useRouter();
   const [loading, setLoading] = useState(false);
   const { toast } = useToast();
-  // const subscription = useSubscriptionStore((state) => state.subscription);
+  const supabase = createSupabaseBrowserClient();
+
+  const session = useSession();
+
+  const [userAccount, setUserAccount] = useState<{
+    fullname: string,
+    email: string,
+    user_groups: string[],
+    user_id: string,
+    avatar: string,
+  } | null>(null);
+
+  async function fetchUserAccount(session: any) {
+    const { data, error } = await supabase.from('users').select('*').eq('email', session.user.email).single();
+    setUserAccount(data);
+  }
+  useEffect(() => {
+
+
+    if (session) {
+      fetchUserAccount(session);
+
+    }
+  }, [session]);
+
 
   const createNewChat = async () => {
-    // //  logic to create a new chat
-    // if (!session?.user.id) return;
+    //  logic to create a new chat
+    if (!session?.user.id || !userAccount) {
+      toast({
+        title: "Error!",
+        description: "You must be signed in to create a new chat!",
+        variant: "destructive",
+      });
+      return;
+    }
 
-    // setLoading(true);
-    // toast({
-    //   title: "Creating a new chat...",
-    //   description: "Hold tight while we create a new chat for you...",
-    //   duration: 3000,
-    // });
+    setLoading(true);
+    toast({
+      title: "Creating a new chat...",
+      description: "Hold tight while we create a new chat for you...",
+      className: "bg-yellow-300 text-black",
+      duration: 3000,
+    });
+
+    const chatGroup = {
+      admin_id: userAccount.user_id,
+      last_text: 'New Chat',
+      last_text_sent_at: Date.now(),
+      last_text_sent_by: userAccount.user_id,
+      last_text_sent_by_details: {
+        user_id: userAccount.user_id,
+        fullname: userAccount.fullname,
+        email: userAccount.email,
+        avatar: userAccount.avatar,
+      },
+      users_with_permission: [userAccount.user_id],
+    }
+
+    const { data, error } = await supabase.from('chat_groups').insert(chatGroup).select('*').single();
+
+    if (error) {
+      console.error(error);
+      toast({
+        title: "Error!",
+        description: "There was an error creating your chat!",
+        variant: "destructive",
+      });
+      setLoading(false);
+      return;
+    }
+
+    // update user_groups in user account
+    const updatedUserGroups = [...userAccount.user_groups, data.id];
+
+    const { error: updatedUserError } = await supabase.from('users').update({ user_groups: updatedUserGroups }).eq('user_id', userAccount.user_id);
+
+    // get updated user account
+    const { data: updatedUser, error: updatedUserError2 } = await supabase.from('users').select('*').eq('user_id', userAccount.user_id).single();
+    setUserAccount(updatedUser);
+
+    if (!error) {
+      setUserAccount(updatedUser);
+      console.log(updatedUserGroups);
+
+      toast({
+        title: "Success!",
+        description: "You have successfully created a new chat!",
+        className: "bg-green-600 text-white",
+        duration: 2000,
+      });
+    }
+
+    // else chat creation failed
+    else {
+      console.error(error);
+      toast({
+        title: "Error!",
+        description: "There was an error creating your chat!",
+        variant: "destructive",
+      });
+    }
+
+
+
 
     // // Check if user is pro and limit them creating a new chat.
 
@@ -95,6 +185,8 @@ function CreateChatButton({ isLarge }: { isLarge?: boolean }) {
     //     setLoading(false);
     //   });
   };
+
+
 
   if (isLarge)
     return (
