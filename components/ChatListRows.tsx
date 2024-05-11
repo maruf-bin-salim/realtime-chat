d1a9ea1b164c516797357b135bb3c1d66eeb24fe
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-05-12 01:07:48 +0600

    realtime group!

--- components/ChatListRows.tsx
+++ components/ChatListRows.tsx
@@ -9,12 +9,13 @@
 import { MessageSquare } from "lucide-react";
 import CreateChatButton from "./CreateChatButton";
 import ChatListRow from "./ChatListRow";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import useSession from "@/lib/supabase/use-session";
+import { createSupabaseBrowserClient } from "@/lib/supabase/browser-client";
 
 // function ChatListRows({ initialChats }: { initialChats: ChatMembers[] }) {
 function ChatListRows({ initialChats }: { initialChats: any[] }) {
   // const { data: session } = useSession();
-  let session: any = null;
 
   // const [members, loading, error] = useCollectionData<ChatMembers>(
   //   session && chatMembersCollectionGroupRef(session?.user.id!),
@@ -23,17 +24,97 @@ function ChatListRows({ initialChats }: { initialChats: any[] }) {
   //   }
   // );
 
-  const members: any = [
-    {
-      chatId: "1111111"
+  const session = useSession();
+  const supabase = createSupabaseBrowserClient();
+  const [userAccount, setUserAccount] = useState<{
+    fullname: string,
+    email: string,
+    user_groups: string[],
+    user_id: string,
+    avatar: string,
+  } | null>(null);
+  const [chatGroups, setChatGroups] = useState<any[]>([]);
+
+
+  // subscribe to chat_groups table and users_with_permissions
+
+  async function getAllUserChatGroups(user_id: String) {
+    const { data, error } = await supabase.from('chat_groups').select('*');
+
+    console.log('all chat groups', data);
+    console.log('user_id', user_id);
+
+
+    if (error) {
+      console.log(error);
+      return;
     }
-  ]
+
+
+
+    let chatGroups = [];
+
+    for (let i = 0; i < data.length; i++) {
+      const chatGroup = data[i];
+      const members_of_chat_group = data[i].users_with_permission || [];
+
+
+      if (members_of_chat_group.includes(user_id)) {
+        chatGroups.push(chatGroup);
+      }
+    }
+
+    setChatGroups(chatGroups);
+
+
+  }
+
+  useEffect(() => {
+    if (session && userAccount) {
+      console.log('getting all user chat groups');
+      getAllUserChatGroups(userAccount.user_id);
+    }
+  }, [userAccount, session]);
+
+  useEffect(() => {
+    async function setUserAccountData(session: any) {
+      const { data, error } = await supabase.from('users').select('*').eq('email', session.user.email).single();
+      console.log('data', data);
+      setUserAccount(data);
+    }
+    if (session) {
+      // update user account
+      setUserAccountData(session);
+    }
+  }
+    , [session]);
+
+  useEffect(() => {
+
+    if (!userAccount) return;
+
+    const subscription = supabase
+      .channel('chat_channel')
+      .on('postgres_changes', { event: '*', schema: 'public', table: 'chat_groups' }, payload => {
+        console.log('Change received!', payload);
+        getAllUserChatGroups(userAccount.user_id);
+      })
+      .subscribe()
+
+    return () => {
+      supabase.removeChannel(subscription);
+    };
+
+  }
+    , [userAccount]);
+
+
 
   const loading: any = false;
   const error: any = false;
 
- 
-  if (members?.length === 0)
+
+  if (chatGroups?.length === 0)
     return (
       <div className="flex flex-col items-center justify-center pt-40 space-y-2">
         <MessageSquare className="h-10 w-10" />
@@ -47,8 +128,8 @@ function ChatListRows({ initialChats }: { initialChats: any[] }) {
 
   return (
     <div className="">
-      {members?.map((member: any, i: any) => {
-        return <ChatListRow key={member.chatId} chatId={member.chatId} />;
+      {chatGroups?.map((member: any, i: any) => {
+        return <ChatListRow key={member.id} chatId={member.id} />;
       })}
     </div>
   );
