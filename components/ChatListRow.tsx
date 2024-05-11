9c01dee64ab0aa44129bc5bfa1b7e46006d6bf12
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-05-12 01:39:00 +0600

    DONE CHAT PAGE

--- components/ChatListRow.tsx
+++ components/ChatListRow.tsx
@@ -1,21 +1,35 @@
 "use client";
 // import { useCollectionData } from "react-firebase-hooks/firestore";
 import { Skeleton } from "./ui/skeleton";
-// import { Message, limitedSortedMessagesRef } from "@/lib/converters/Message";
+// import { chatGroup, limitedSortedgroupsRef } from "@/lib/converters/chatGroup";
 import UserAvatar from "./UserAvatar";
 // import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useLanguageStore } from "@/store/store";
 
-function ChatListRow({ chatId }: { chatId: string }) {
-  // const [messages, loading, error] = useCollectionData<Message>(
-  //   limitedSortedMessagesRef(chatId)
+function ChatListRow({ chatId, chatGroup }: { chatId: string, chatGroup: any }) {
+  // const [groups, loading, error] = useCollectionData<chatGroup>(
+  //   limitedSortedgroupsRef(chatId)
   // );
 
-  const messages: any = [
+  // const chatGroup = {
+  //   admin_id: userAccount.user_id,
+  //   last_text: 'New Chat',
+  //   last_text_sent_at: Date.now(),
+  //   last_text_sent_by: userAccount.user_id,
+  //   last_text_sent_by_details: {
+  //     user_id: userAccount.user_id,
+  //     fullname: userAccount.fullname,
+  //     email: userAccount.email,
+  //     avatar: userAccount.avatar,
+  //   },
+  //   users_with_permission: [userAccount.user_id],
+  // }
+
+  const groups: any = [
     {
       user: {
-        name: "John Doe",
+        name: chatGroup?.name || "John Doe1",
         image: "/images/avatar.jpg",
       },
       translated: {
@@ -24,7 +38,6 @@ function ChatListRow({ chatId }: { chatId: string }) {
       timestamp: new Date(),
     },
   ];
-  const loading: any = false;
   const error: any = false;
 
   const language = useLanguageStore((state) => state.language);
@@ -36,54 +49,54 @@ function ChatListRow({ chatId }: { chatId: string }) {
     return chatId.substring(0, n);
   }
 
-  const row = (message?: any) => (
+  function trunchateName(name: string) {
+
+    // keep 2 words or 15 characters, whichever is less
+    const words = name.split(" ");
+    if (words.length > 1) {
+      return words[0] + " " + words[1];
+    }
+    return name.substring(0, 15);
+    
+  }
+
+
+  return (
     <div
       key={chatId}
-      onClick={() => router.push(`/chat/${chatId}`)}
+      onClick={() => router.push(`/chat/${chatGroup?.id}`)}
       className="flex p-5 items-center space-x-2 cursor-pointer hover:bg-gray-100 hover:rounded-2xl dark:hover:bg-slate-700 dark:hover:bg-opacity-20 "
     >
       <UserAvatar
-        name={message?.user.name || session?.user.name}
-        image={message?.user.image || session?.user.image}
+        name={chatGroup?.last_text_sent_by_details.fullname || 'User'}
+        image={chatGroup?.last_text_sent_by_details.avatar || "/images/avatar.jpg"}
       />
 
       <div className="flex-1">
         <p className="font-bold">
-          {!message && "New Chat"}
-          {message &&
-            [message?.user.name || session?.user.name].toString().split(" ")[0]}
+          {chatGroup.last_text === "New Chat" && "New Chat "}
+          <span className="text-gray-400">
+            {chatGroup && trunchateName(chatGroup.last_text_sent_by_details.fullname) || "User"}
+          </span>
         </p>
         <p className="text-gray-400 line-clamp-1 w-40 md:w-96">
-          {message?.translated?.[language] || "Get the conversation started!"}
+          {
+            chatGroup?.last_text === "New Chat"
+              ? "Get the conversation started!"
+              : chatGroup?.last_text
+          }
+          {/* {chatGroup?.translated?.[language] || "Get the conversation started!"} */}
         </p>
       </div>
 
       <div className="text-xs text-gray-400 text-right">
         <p className="mb-auto">
-          {message
-            ? new Date(message.timestamp).toLocaleDateString()
-            : "No messages yet"}
+          {chatGroup
+            ? new Date(chatGroup.last_text_sent_at).toLocaleDateString()
+            : "No groups yet"}
         </p>
         <p className="">chat #{prettyUUID()}</p>
       </div>
-    </div>
-  );
-
-  return (
-    <div>
-      {loading && (
-        <div className="flex p-5 items-center space-x-2">
-          <Skeleton className="h-12 w-12 rounded-full" />
-          <div className="space-y-2 flex-1">
-            <Skeleton className="h-4 w-full" />
-            <Skeleton className="h-4 w-1/4" />
-          </div>
-        </div>
-      )}
-
-      {messages?.length === 0 && !loading && row()}
-
-      {messages?.map((message: any) => row(message))}
     </div>
   );
 }
--- components/ChatListRows.tsx
+++ components/ChatListRows.tsx
@@ -12,6 +12,7 @@ import ChatListRow from "./ChatListRow";
 import { useEffect, useState } from "react";
 import useSession from "@/lib/supabase/use-session";
 import { createSupabaseBrowserClient } from "@/lib/supabase/browser-client";
+import { Skeleton } from "./ui/skeleton";
 
 // function ChatListRows({ initialChats }: { initialChats: ChatMembers[] }) {
 function ChatListRows({ initialChats }: { initialChats: any[] }) {
@@ -34,6 +35,7 @@ function ChatListRows({ initialChats }: { initialChats: any[] }) {
     avatar: string,
   } | null>(null);
   const [chatGroups, setChatGroups] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
 
 
   // subscribe to chat_groups table and users_with_permissions
@@ -46,6 +48,7 @@ function ChatListRows({ initialChats }: { initialChats: any[] }) {
 
 
     if (error) {
+      setLoading(false);
       console.log(error);
       return;
     }
@@ -65,6 +68,7 @@ function ChatListRows({ initialChats }: { initialChats: any[] }) {
     }
 
     setChatGroups(chatGroups);
+    setLoading(false);
 
 
   }
@@ -72,12 +76,15 @@ function ChatListRows({ initialChats }: { initialChats: any[] }) {
   useEffect(() => {
     if (session && userAccount) {
       console.log('getting all user chat groups');
+
       getAllUserChatGroups(userAccount.user_id);
+
     }
   }, [userAccount, session]);
 
   useEffect(() => {
     async function setUserAccountData(session: any) {
+      setLoading(true);
       const { data, error } = await supabase.from('users').select('*').eq('email', session.user.email).single();
       console.log('data', data);
       setUserAccount(data);
@@ -97,6 +104,7 @@ function ChatListRows({ initialChats }: { initialChats: any[] }) {
       .channel('chat_channel')
       .on('postgres_changes', { event: '*', schema: 'public', table: 'chat_groups' }, payload => {
         console.log('Change received!', payload);
+        setLoading(true);
         getAllUserChatGroups(userAccount.user_id);
       })
       .subscribe()
@@ -109,27 +117,31 @@ function ChatListRows({ initialChats }: { initialChats: any[] }) {
     , [userAccount]);
 
 
-
-  const loading: any = false;
-  const error: any = false;
-
-
-  if (chatGroups?.length === 0)
-    return (
-      <div className="flex flex-col items-center justify-center pt-40 space-y-2">
-        <MessageSquare className="h-10 w-10" />
-        <h1 className="text-5xl font-extralight">Welcome!</h1>
-        <h2 className="pb-10">
-          {`Let's get you started by creating your first chat!`}
-        </h2>
-        <CreateChatButton isLarge />
+  if (loading) return (
+    <div className="flex p-5 items-center space-x-2">
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="space-y-2 flex-1">
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-1/4" />
       </div>
-    );
+    </div>
+  )
+
+  if (chatGroups?.length === 0) return (
+    <div className="flex flex-col items-center justify-center pt-40 space-y-2">
+      <MessageSquare className="h-10 w-10" />
+      <h1 className="text-5xl font-extralight">Welcome!</h1>
+      <h2 className="pb-10">
+        {`Let's get you started by creating your first chat!`}
+      </h2>
+      <CreateChatButton isLarge />
+    </div>
+  )
 
   return (
     <div className="">
       {chatGroups?.map((member: any, i: any) => {
-        return <ChatListRow key={member.id} chatId={member.id} />;
+        return <ChatListRow key={member.id} chatId={member.id} chatGroup={member} />;
       })}
     </div>
   );
